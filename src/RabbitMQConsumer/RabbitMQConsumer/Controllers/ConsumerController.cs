fed3918377dda928a8b7524f1f242a906d96d170
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-07 21:51:22 -0300

    Controller Consumer

---
+++ src/RabbitMQConsumer/RabbitMQConsumer/Controllers/ConsumerController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQConsumer.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQConsumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetMessage()
+        {
+            var response = new List<Mensagem>();
+            var msg = new Mensagem();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "minhafila1",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        msg = JsonSerializer.Deserialize<Mensagem>(message);
+
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+                        response.Add(msg);
+                        Console.WriteLine("========================================================");
+                        Console.WriteLine($"Mensagem id:{msg.Id}, descriçao:{msg.Descricao}, ");
+                        Console.WriteLine("========================================================\n");
+                    }
+                    catch (Exception e)
+                    {
+                        channel.BasicNack(deliveryTag: ea.DeliveryTag, false, true);
+                        throw e;
+                    }
+                };
+
+                channel.BasicConsume(queue: "minhafila1",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+
+                return Ok(response);
+            }
+        }
+    }
+}
